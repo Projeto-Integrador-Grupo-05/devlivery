26154704fd73d4a226d03aad625f0b1b4f6fbfed
viniaquino <contato.viniaquino@outlook.com>
2024-12-17 13:26:21 -0300

    feat: implementação da security e método especial

---
+++ src/main/java/com/generation/devlivery/model/Produto.java
@@ -0,0 +1,78 @@
+package com.generation.devlivery.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduto;  
+    
+    private String nomeProduto;
+
+    private Integer calorias; 
+
+    private Boolean saudavel; 
+
+    private BigDecimal preco;
+//  @ManyToOne
+//	@JsonIgnoreProperties("produtos")
+//	private Categoria categoria;
+
+    
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public Integer getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(Integer calorias) {
+        this.calorias = calorias;
+    }
+
+    public Boolean getSaudavel() {
+        return saudavel;
+    }
+
+    public void setTipo(Boolean tipo) {
+        this.saudavel = tipo;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+    
+	/*public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}*/
+}
