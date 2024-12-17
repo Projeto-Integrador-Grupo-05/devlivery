f145e1e23ee0a9ee93153db4824e1fe745520c31
Rafassz <rafassz.aps@gmail.com>
2024-12-17 19:55:16 -0300

    Atualização pom

--- src/main/java/com/generation/devlivery/model/Usuario.java
+++ src/main/java/com/generation/devlivery/model/Usuario.java
@@ -1,15 +1,10 @@
 package com.generation.devlivery.model;
 
-import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.CascadeType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -25,81 +20,69 @@ public class Usuario {
 
 	@NotBlank(message = "O Atributo Nome é Obrigatório!")
 	private String nome;
-	
+
 	@Schema(example = "email@email.com.br")
-	@NotBlank (message = "O Atributo E-mail é Obrigatório!")
-	@Email (message = "O Atributo Usuário deve ser um email válido!")
+	@NotBlank(message = "O Atributo E-mail é Obrigatório!")
+	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String email;
-	
-	@NotBlank (message = "O Atributo Senha é Obrigatório!")
+
+	@NotBlank(message = "O Atributo Senha é Obrigatório!")
 	@Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
 	private String senha;
-	
+
 	@NotBlank(message = "O tipo de usuário é obrigatório!")
 	private String tipo;
-	
-	
+
 	@NotBlank(message = "O endereço é obrigatório!")
 	@Size(min = 5, max = 255, message = "O endereço deve ter entre 5 e 255 caracteres")
 	private String endereco;
-	
+
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getNome() {
 		return nome;
 	}
-
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getSenha() {
 		return senha;
 	}
-
 
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
-
 	public String getTipo() {
 		return tipo;
 	}
-
 
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
 
-
 	public String getEndereco() {
 		return endereco;
 	}
 
-
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-	
+
 }
