c5d621452e1d3bb095fb0db3f1cd1d2de7c4d274
ninguemzin <alm1427@hotmail.com>
2024-12-17 09:13:46 -0300

    first commit

---
+++ src/main/java/com/generation/devlivery/DevliveryApplication.java
@@ -0,0 +1,13 @@
+package com.generation.devlivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/devlivery/DevliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.devlivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
